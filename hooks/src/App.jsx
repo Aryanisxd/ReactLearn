c6b1ef8935476af8326d407746528183d2c7d8cd
Aryan sharma <officearyan14@gmail.com>
2025-07-09 17:34:15 +0530

    intialised currency project

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,3 +1,37 @@
+// import { useEffect, useState , useRef } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   const value = useRef(0)
+
+
+// useEffect(()=>{
+//   value.current = value.current + 1
+// })
+
+
+//   return (
+//     <>
+//       <button onClick={() => setCount(count + 1)}>
+//         +1
+//       </button>
+//       <h3>{count}</h3>
+//       <button onClick={() => setCount(count - 1)}>-1</button>
+
+//       <h3>the component is rendered this time {value.current} </h3>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+//now working on use memo ========================================================
 import { useEffect, useState , useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,7 +41,7 @@ function App() {
   const [count, setCount] = useState(0)
   const value = useRef(0)
 
-
+``
 useEffect(()=>{
   value.current = value.current + 1
 })
